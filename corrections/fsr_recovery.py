03715af4c013df624f1b10c2d70e5feae9915107
green-cabbage <hyeonseo_yun@hotmail.com>
2024-01-28 23:31:01 +0100

    additing typevar

--- corrections/fsr_recovery.py
+++ corrections/fsr_recovery.py
@@ -1,8 +1,11 @@
 import numpy as np
 import awkward as ak
+from typing import TypeVar, Tuple
+ak_array = TypeVar('ak_array')
+coffea_nanoevent = TypeVar('coffea_nanoevent') 
 
 
-def fsr_recovery(events):
+def fsr_recovery(events: coffea_nanoevent) -> ak_array:
     print(f"fsr_recovery type(events): {type(events)}")
     fsrPhotonsToRecover = (
         (events.Muon.fsrPhotonIdx >= 0)
@@ -40,6 +43,9 @@ def fsr_recovery(events):
     print(f"fsr_recovery events.Muon.mass_fsr:[fsrPhotonsToRecover] \n {ak.to_numpy(ak.flatten(events.Muon.mass_fsr[fsrPhotonsToRecover]))}")
     print(f"fsr_recovery events.Muon.iso_fsr[fsrPhotonsToRecover]: \n {ak.to_numpy(ak.flatten(events.Muon.iso_fsr[fsrPhotonsToRecover]))}")
     print(f"fsr_recovery ak.where(ak.sum(fsrPhotonsToRecover, axis=1)>0): \n {ak.to_numpy(ak.where(ak.sum(fsrPhotonsToRecover, axis=1)>0))}")
+    print(f"fsr_recovery ak.sum(fsrPhotonsToRecover): {ak.sum(fsrPhotonsToRecover)}")
+    
+    return fsrPhotonsToRecover
 
 
 
