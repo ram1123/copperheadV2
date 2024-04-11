f6f8c3f04c483ae30854704b80709d9278d10e00
green-cabbage <hyeonseo_yun@hotmail.com>
2024-04-11 19:08:14 +0200

    updating run 3 parameters

--- make_parameters.py
+++ make_parameters.py
@@ -3,7 +3,7 @@ import os
 import argparse
 
 def for_all_years(value):
-    out = {k: value for k in ["2016preVFP","2016postVFP", "2017", "2018"]}
+    out = {k: value for k in ["2016preVFP","2016postVFP", "2017", "2018","2022EE"]}
     return out
 
 def get_variations(sources):
@@ -24,21 +24,21 @@ parameters.update(
         "muon_id": for_all_years("mediumId"),
         # "muon_flags": for_all_years(["isGlobal", "isTracker"]),
         "muon_flags": for_all_years([]),
-        "muon_leading_pt": {"2016preVFP": 26.0,"2016postVFP": 26.0, "2017": 29.0, "2018": 26.0},
+        "muon_leading_pt": {"2016preVFP": 26.0,"2016postVFP": 26.0, "2017": 29.0, "2018": 26.0, "2022EE": 26.0,},
         "muon_trigmatch_iso": for_all_years(0.15),  # tight iso
         "muon_trigmatch_dr": for_all_years(0.1),
         "muon_trigmatch_id": for_all_years("tightId"),
         "electron_pt_cut": for_all_years(20.0),
         "electron_eta_cut": for_all_years(2.5),
+        # "electron_id": for_all_years("mvaIso_WP90"), #Run3 ready!
         "electron_id": for_all_years("mvaFall17V2Iso_WP90"),
         "jet_pt_cut": for_all_years(25.0),
         "jet_eta_cut": for_all_years(4.7),
-        "jet_id": {"2016preVFP": "loose","2016postVFP": "loose", "2017": "tight", "2018": "tight"},
-        # "jet_puid": {"2016preVFP": "loose","2016postVFP": "loose", "2017": "loose", "2018": "loose"},
-        "jet_puid": {"2016preVFP": "loose","2016postVFP": "loose", "2017": "2017corrected", "2018": "loose"},
+        "jet_id": {"2016preVFP": "loose","2016postVFP": "loose", "2017": "tight", "2018": "tight","2022EE": "tight",},
+        "jet_puid": {"2016preVFP": "loose","2016postVFP": "loose", "2017": "loose", "2018": "loose", "2022EE": "loose",},
         "min_dr_mu_jet": for_all_years(0.4),
-        "btag_loose_wp": {"2016preVFP": 0.2027,"2016postVFP": 0.1918 ,"2017": 0.1355, "2018": 0.1208},
-        "btag_medium_wp": {"2016preVFP": 0.6001,"2016postVFP": 0.4847, "2017": 0.4506, "2018": 0.4168},
+        "btag_loose_wp": {"2016preVFP": 0.2027,"2016postVFP": 0.1918 ,"2017": 0.1355, "2018": 0.0490,"2022EE":0.1200,}, ## fix 2022
+        "btag_medium_wp": {"2016preVFP": 0.6001,"2016postVFP": 0.4847, "2017": 0.4506, "2018": 0.4168, "2022EE":0.4168,}, ## fix 2022
         "softjet_dr2": for_all_years(0.16),
     }
 )
@@ -48,6 +48,7 @@ parameters["lumimask"] = {
     "2016postVFP": "data/lumimasks/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt",
     "2017": "data/lumimasks/Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt",
     "2018": "data/lumimasks/Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt",
+    "2022EE": "data/lumimasks/Cert_Collisions2022_355100_362760_Golden.txt",
 }
 
 parameters["hlt"] = {
@@ -55,6 +56,7 @@ parameters["hlt"] = {
     "2016postVFP": ["IsoMu24", "IsoTkMu24"],
     "2017": ["IsoMu27"],
     "2018": ["IsoMu24"],
+    "2022EE": ["IsoMu24"],
 }
 
 parameters["roccor_file"] = {
@@ -62,6 +64,7 @@ parameters["roccor_file"] = {
     "2016postVFP": "data/roch_corr/RoccoR2016bUL.txt",
     "2017": "data/roch_corr/RoccoR2017UL.txt",
     "2018": "data/roch_corr/RoccoR2018UL.txt",
+    "2022EE": "data/roch_corr/RoccoR2018UL.txt",
 }
 
 parameters["nnlops_file"] = for_all_years("data/NNLOPS_reweight.root")
@@ -79,6 +82,7 @@ parameters["btag_sf_json"] = {
     "2016postVFP": "/cvmfs/cms.cern.ch/rsync/cms-nanoAOD/jsonpog-integration/POG/BTV/2016postVFP_UL/btagging.json.gz",
     "2017": "/cvmfs/cms.cern.ch/rsync/cms-nanoAOD/jsonpog-integration/POG/BTV/2017_UL/btagging.json.gz",
     "2018": "/cvmfs/cms.cern.ch/rsync/cms-nanoAOD/jsonpog-integration/POG/BTV/2018_UL/btagging.json.gz",
+    "2022EE": "/cvmfs/cms.cern.ch/rsync/cms-nanoAOD/jsonpog-integration/POG/BTV/2022_Summer22EE/btagging.json.gz",
 }
 parameters["btag_sf_csv"] = {
 
@@ -86,6 +90,7 @@ parameters["btag_sf_csv"] = {
     "2016postVFP": "data/btag/DeepCSV_2016LegacySF_V1.csv",
     "2017": "data/btag/DeepCSV_106XUL17SF.csv",
     "2018": "data/btag/DeepCSV_106XUL18SF.csv",
+    "2022EE": "data/btag/DeepCSV_106XUL18SF.csv",
 }
 
 parameters["pu_file_data"] = {
@@ -93,6 +98,7 @@ parameters["pu_file_data"] = {
     "2016postVFP": "data/pileup/puData2016_UL_withVar.root",
     "2017": "data/pileup/puData2017_UL_withVar.root",
     "2018": "data/pileup/puData2018_UL_withVar.root",
+    "2022EE": "data/pileup/puData2018_UL_withVar.root",
 }
 
 parameters["pu_file_mc"] = {
@@ -100,6 +106,7 @@ parameters["pu_file_mc"] = {
     "2016postVFP": "data/pileup/pileup_profile_Summer16.root",
     "2017": "data/pileup/mcPileup2017.root",
     "2018": "data/pileup/mcPileup2018.root",
+    "2022EE": "data/pileup/mcPileup2018.root",
 }
 
 parameters["muSFFileList"] = {
@@ -191,6 +198,24 @@ parameters["muSFFileList"] = {
             "scale": 50.79 / 59.74,
         },
     ],
+        "2022EE": [
+        {
+            "id": (
+                "data/muon_sf/year2017/MuonSF_Run2017_UL_ID.root",
+                "NUM_MediumID_DEN_TrackerMuons_abseta_pt",
+            ),
+            "iso": (
+                "data/muon_sf/year2017/MuonSF_Run2017_UL_ISO.root",
+                "NUM_TightRelIso_DEN_MediumID_abseta_pt",
+            ),
+            "trig": (
+                "data/muon_sf/mu2017/EfficienciesAndSF_RunBtoF_Nov17Nov2017.root",
+                "IsoMu27_PtEtaBins/efficienciesDATA/abseta_pt_DATA",
+                "IsoMu27_PtEtaBins/efficienciesMC/abseta_pt_MC",
+            ),
+            "scale": 1.0,
+        }
+        ],
 }
 
 parameters["zpt_weights_file"] = for_all_years("data/zpt_weights.histo.json")
@@ -239,13 +264,13 @@ parameters.update(
                 "BadChargedCandidateFilter",
             ]
         ),
-        "do_l1prefiring_wgts": {"2016preVFP": True,"2016postVFP": True, "2017": True, "2018": False},
+        "do_l1prefiring_wgts": {"2016preVFP": True,"2016postVFP": True, "2017": True, "2018": False,"2022EE": False},
     }
 )
 
-parameters["n_pdf_variations"] = {"2016preVFP": 100, "2016postVFP": 100, "2017": 33, "2018": 33}
+parameters["n_pdf_variations"] = {"2016preVFP": 100, "2016postVFP": 100, "2017": 33, "2018": 33,"2022EE":33}
 
-parameters["dnn_max"] = {"2016preVFP": 1.75, "2016postVFP": 1.75, "2017": 2.0, "2018": 2.35}
+parameters["dnn_max"] = {"2016preVFP": 1.75, "2016postVFP": 1.75, "2017": 2.0, "2018": 2.35,"2022EE": 2.35}
 
 
 cross_sections = {
