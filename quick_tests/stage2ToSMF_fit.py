ab857024672854e020ce59fd907d1b2d7dfaf1ec
green-cabbage <hyeonseo_yun@hotmail.com>
2024-06-27 21:03:45 +0200

    adding run_stage2.py in quick_test. probably should move it to the main directory later

--- quick_tests/stage2ToSMF_fit.py
+++ quick_tests/stage2ToSMF_fit.py
@@ -14,70 +14,77 @@ from quickSMFtest_functions import MakeBWZ_Redux, MakeBWZxBern, MakeSumExponenti
 
 
 if __name__ == "__main__":
-    client =  Client(n_workers=31,  threads_per_worker=1, processes=True, memory_limit='4 GiB') 
-    load_path = "/depot/cms/users/yun79/results/stage1/test_VBF-filter_JECon_07June2024/2018/f1_0"
-    # full_load_path = load_path+f"/data_C/*/*.parquet"
-    # full_load_path = load_path+f"/data_D/*/*.parquet"
-    # full_load_path = load_path+f"/data_*/*/*.parquet"
-    full_load_path = load_path+f"/data_A/*/*.parquet"
-    events = dak.from_parquet(full_load_path)
+    """
+    laoding stage1 output data and evalauting BDT is deletegated to run_stage2.py
+    """
+    # client =  Client(n_workers=31,  threads_per_worker=1, processes=True, memory_limit='4 GiB') 
+    # load_path = "/depot/cms/users/yun79/results/stage1/test_VBF-filter_JECon_07June2024/2018/f1_0"
+    # # full_load_path = load_path+f"/data_C/*/*.parquet"
+    # # full_load_path = load_path+f"/data_D/*/*.parquet"
+    # # full_load_path = load_path+f"/data_*/*/*.parquet"
+    # full_load_path = load_path+f"/data_A/*/*.parquet"
+    # events = dak.from_parquet(full_load_path)
 
-    # load and obtain MVA outputs
-    events["dimuon_dEta"] = np.abs(events.mu1_pt -events.mu2_pt)
-    events["dimuon_pt_log"] = np.log(events.dimuon_pt)
-    events["jj_mass_log"] = np.log(events.jj_mass)
-    events["ll_zstar_log"] = np.log(events.ll_zstar)
-    events["mu1_pt_over_mass"] = events.mu1_pt / events.dimuon_mass
-    events["mu2_pt_over_mass"] = events.mu2_pt / events.dimuon_mass
+    # # load and obtain MVA outputs
+    # events["dimuon_dEta"] = np.abs(events.mu1_pt -events.mu2_pt)
+    # events["dimuon_pt_log"] = np.log(events.dimuon_pt)
+    # events["jj_mass_log"] = np.log(events.jj_mass)
+    # events["ll_zstar_log"] = np.log(events.ll_zstar)
+    # events["mu1_pt_over_mass"] = events.mu1_pt / events.dimuon_mass
+    # events["mu2_pt_over_mass"] = events.mu2_pt / events.dimuon_mass
     
     
-    training_features = [
-        'dimuon_cos_theta_cs', 'dimuon_dEta', 'dimuon_dPhi', 'dimuon_dR', 'dimuon_eta', 'dimuon_phi', 'dimuon_phi_cs', 'dimuon_pt', 
-        'dimuon_pt_log', 'jet1_eta', 'jet1_phi', 'jet1_pt', 'jet1_qgl', 'jet2_eta', 'jet2_phi', 
-        'jet2_pt', 'jet2_qgl', 'jj_dEta', 'jj_dPhi', 'jj_eta', 'jj_mass', 'jj_mass_log', 
-        'jj_phi', 'jj_pt', 'll_zstar_log', 'mmj1_dEta', 'mmj1_dPhi', 'mmj2_dEta', 'mmj2_dPhi', 
-        'mmj_min_dEta', 'mmj_min_dPhi', 'mmjj_eta', 'mmjj_mass', 'mmjj_phi', 'mmjj_pt', 'mu1_eta', 'mu1_iso', 
-        'mu1_phi', 'mu1_pt_over_mass', 'mu2_eta', 'mu2_iso', 'mu2_phi', 'mu2_pt_over_mass', 'zeppenfeld'
-    ]
-    for training_feature in training_features:
-        if training_feature not in events.fields:
-            print(f"mssing feature: {training_feature}")
+    # training_features = [
+    #     'dimuon_cos_theta_cs', 'dimuon_dEta', 'dimuon_dPhi', 'dimuon_dR', 'dimuon_eta', 'dimuon_phi', 'dimuon_phi_cs', 'dimuon_pt', 
+    #     'dimuon_pt_log', 'jet1_eta', 'jet1_phi', 'jet1_pt', 'jet1_qgl', 'jet2_eta', 'jet2_phi', 
+    #     'jet2_pt', 'jet2_qgl', 'jj_dEta', 'jj_dPhi', 'jj_eta', 'jj_mass', 'jj_mass_log', 
+    #     'jj_phi', 'jj_pt', 'll_zstar_log', 'mmj1_dEta', 'mmj1_dPhi', 'mmj2_dEta', 'mmj2_dPhi', 
+    #     'mmj_min_dEta', 'mmj_min_dPhi', 'mmjj_eta', 'mmjj_mass', 'mmjj_phi', 'mmjj_pt', 'mu1_eta', 'mu1_iso', 
+    #     'mu1_phi', 'mu1_pt_over_mass', 'mu2_eta', 'mu2_iso', 'mu2_phi', 'mu2_pt_over_mass', 'zeppenfeld'
+    # ]
+    # for training_feature in training_features:
+    #     if training_feature not in events.fields:
+    #         print(f"mssing feature: {training_feature}")
     
-    fields2load = training_features + ["h_peak", "h_sidebands", "dimuon_mass", "wgt_nominal_total"]
-    events = events[fields2load]
-    # load data to memory using compute()
-    events = ak.zip({
-        field : events[field] for field in events.fields
-    }).compute()
+    # fields2load = training_features + ["h_peak", "h_sidebands", "dimuon_mass", "wgt_nominal_total"]
+    # events = events[fields2load]
+    # # load data to memory using compute()
+    # events = ak.zip({
+    #     field : events[field] for field in events.fields
+    # }).compute()
 
 
-    parameters = {
-    "models_path" : "/depot/cms/hmm/vscheure/data/trained_models/"
-    }
-    # model_name = "BDTv12_2018"
-    # model_name = "phifixedBDT_2018"
-    model_name = "BDTperyear_2018"
+    # parameters = {
+    # "models_path" : "/depot/cms/hmm/vscheure/data/trained_models/"
+    # }
+    # # model_name = "BDTv12_2018"
+    # # model_name = "phifixedBDT_2018"
+    # model_name = "BDTperyear_2018"
     
-    processed_events = evaluate_bdt(events, "nominal", model_name, parameters)
+    # processed_events = evaluate_bdt(events, "nominal", model_name, parameters)
 
-    # load BDT score edges for subcategory divison
-    BDTedges_load_path = "../configs/MVA/ggH/BDT_edges.yaml"
-    edges = OmegaConf.load(BDTedges_load_path)
-    year = "2018"
-    edges = np.array(edges[year])
+    # # load BDT score edges for subcategory divison
+    # BDTedges_load_path = "../configs/MVA/ggH/BDT_edges.yaml"
+    # edges = OmegaConf.load(BDTedges_load_path)
+    # year = "2018"
+    # edges = np.array(edges[year])
 
-    # Calculate the subCategory index 
-    BDT_score = processed_events["BDT_score"]
-    n_edges = len(edges)
-    BDT_score_repeat = ak.concatenate([BDT_score[:,np.newaxis] for i in range(n_edges)], axis=1)
-    # BDT_score_repeat
-    n_rows = len(BDT_score_repeat)
-    edges_repeat = np.repeat(edges[np.newaxis,:],n_rows,axis=0)
-    # edges_repeat.shape
-    edge_idx = ak.sum( (BDT_score_repeat >= edges_repeat), axis=1)
-    subCat_idx =  edge_idx - 1 # sub category index starts at zero
-    processed_events["subCategory_idx"] = subCat_idx
+    # # Calculate the subCategory index 
+    # BDT_score = processed_events["BDT_score"]
+    # n_edges = len(edges)
+    # BDT_score_repeat = ak.concatenate([BDT_score[:,np.newaxis] for i in range(n_edges)], axis=1)
+    # # BDT_score_repeat
+    # n_rows = len(BDT_score_repeat)
+    # edges_repeat = np.repeat(edges[np.newaxis,:],n_rows,axis=0)
+    # # edges_repeat.shape
+    # edge_idx = ak.sum( (BDT_score_repeat >= edges_repeat), axis=1)
+    # subCat_idx =  edge_idx - 1 # sub category index starts at zero
+    # processed_events["subCategory_idx"] = subCat_idx
 
+    load_path = "/work/users/yun79/stage2_output/test/processed_events.parquet"
+    processed_events = ak.from_parquet(load_path)
+    print("events loaded!")
+    
     # comence roofit fitting for each subcategory 
     n_subCats = 5
     poly_order_by_cat = {
@@ -137,7 +144,7 @@ if __name__ == "__main__":
         print(f"smf_order: {smf_order}")
         for ix in range(smf_order-1): # minus one bc the normalization constraint takes off one degree of freedom
             name = f"smf_{ix}"
-            smf_coeff = rt.RooRealVar(name, name, 0.005, 0, 1)
+            smf_coeff = rt.RooRealVar(name, name, 0.05, -0.5, 0.5)
             smfVarList.append(smf_coeff)
     
 
