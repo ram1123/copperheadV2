2f417adfd18b26ac8db662eefc30daf8ba7a78e5
green-cabbage <hyeonseo_yun@hotmail.com>
2024-07-23 00:55:33 +0200

    Fitting over all the subcategory seems to work. Just need to properly plot it.

--- quick_tests/quick_simultanFitting.py
+++ quick_tests/quick_simultanFitting.py
@@ -9,7 +9,7 @@ from typing import Tuple, List, Dict
 import ROOT as rt
 
 
-from quickSMFtest_functions import MakeBWZ_Redux
+from quickSMFtest_functions import MakeBWZ_Redux, MakeFEWZxBern, MakeSumExponential
 
 if __name__ == "__main__":
     """
@@ -33,18 +33,33 @@ if __name__ == "__main__":
     subCatColorMap = {
         "subCat0":rt.kBlue,
         "subCat1":rt.kGreen,
+        "subCat2":rt.kRed,
+        "subCat3":rt.kYellow,
+        "subCat4":rt.kOrange,
     }
     dof = 3 # degrees of freedom for the core-functions. This should be same for all the functions
     smf_coeffStartVals = {
         0: {
-            1: 0.1, # smf val start with index one bc zeroth order coeff is assumed to be zero for RooChebychev
-            2: 0.01,
-            3: -0.019,
+            1: 0.6, # smf val start with index one bc zeroth order coeff is assumed to be zero for RooChebychev
+            2: 0.2,
+            3: 0.0005,
         },
         1: {
-            1: -0.05,
-            2: 0.02,
-            3: -0.01,
+            1: 0.5,
+            2: 0.2,
+            3: -0.002,
+        },
+        2: {
+            1: 0.4,
+            2: 0.2,
+        },
+        3: {
+            1: 0.4,
+            2: 0.2,
+        },
+        4: {
+            1: 0.4,
+            2: 0.2,
         },
     }
     
@@ -71,45 +86,60 @@ if __name__ == "__main__":
 
     # intialize the core functions 
     BWZ_Redux, params_redux =  MakeBWZ_Redux(mass, dof)
+    FEWZxBern, params_fewz = MakeFEWZxBern(mass, dof)
+    sumExp, params_exp = MakeSumExponential(mass, dof)
+
+    core_models = [BWZ_Redux, FEWZxBern, sumExp]
     
     # for cat_ix in range(5):
-    for cat_ix in [0,1]:
+    for cat_ix in [0]:
         subCat_name = f"subCat{cat_ix}"
-        sample.defineType(subCat_name)
-        # generate
-        subCat_filter = (processed_eventsData["subCategory_idx"] == cat_ix)
-        subCat_mass_arr = processed_eventsData.dimuon_mass[subCat_filter]
-        subCat_mass_arr  = ak.to_numpy(subCat_mass_arr) # convert to numpy for rt.RooDataSet
-        roo_datasetData = rt.RooDataSet.from_numpy({mass_name: subCat_mass_arr}, [mass])
-        roo_datasetData.SetName(subCat_name+"_Dataset_Data")
-        roo_histData = rt.RooDataHist(subCat_name+"_DataHist_Data",f"binned version of roo_datasetData of subcat {cat_ix}", rt.RooArgSet(mass), roo_datasetData)
-        print(f"roo_datasetData.GetName(): {roo_datasetData.GetName()}")
-
         smfVarList = []
         smf_order= poly_order_by_cat[cat_ix]
         print(f"smf_order: {smf_order}")
         for ix in range(smf_order): 
-            name = subCat_name+f"SMF_Coeff{ix+1}"
-            start_val = smf_coeffStartVals[cat_ix][ix+1]
+            name = subCat_name+f"_SMF_Coeff{ix+1}"
+            try:
+                start_val = smf_coeffStartVals[cat_ix][ix+1]
+            except:
+                start_val = -0.005
             smf_coeff = rt.RooRealVar(name, name,  start_val, -10, 10)
+                
             smfVarList.append(smf_coeff)
-    
         name = subCat_name + "_SMF"
         polynomial_model = rt.RooChebychev(name, name, mass, smfVarList)
-        name = subCat_name+f"_SMFx{BWZ_Redux.GetName()}"
-        final_BWZ_Redux = rt.RooProdPdf(name, name, [polynomial_model,BWZ_Redux])
-        # final_BWZ_Redux.Print("v")
-        
-        # add the information to relevant dictionaries
-        model_dict[subCat_name] = final_BWZ_Redux
-        subCat_dataHists[subCat_name] = roo_histData
-        misc_dict[roo_datasetData.GetName()] = roo_datasetData
+
+        # add model and variable info so they don't get deleted
         misc_dict[polynomial_model.GetName()] = polynomial_model
         for smf_coeff in smfVarList:
             misc_dict[smf_coeff.GetName()] = smf_coeff
-        # misc_dict.update(params_redux)
+
+        
+        for core_model in core_models:
+            subCat_name4coreModel = subCat_name + "_" + core_model.GetName()
+            sample.defineType(subCat_name4coreModel)
+            # generate
+            subCat_filter = (processed_eventsData["subCategory_idx"] == cat_ix)
+            subCat_mass_arr = processed_eventsData.dimuon_mass[subCat_filter]
+            subCat_mass_arr  = ak.to_numpy(subCat_mass_arr) # convert to numpy for rt.RooDataSet
+            roo_datasetData = rt.RooDataSet.from_numpy({mass_name: subCat_mass_arr}, [mass])
+            roo_datasetData.SetName(subCat_name4coreModel+"_Dataset_Data")
+            roo_histData = rt.RooDataHist(subCat_name4coreModel+"_DataHist_Data",f"binned version of roo_datasetData of subcat {cat_ix}", rt.RooArgSet(mass), roo_datasetData)
+            print(f"roo_datasetData.GetName(): {roo_datasetData.GetName()}")
+    
+            
+            name = subCat_name4coreModel+f"_SMFx{BWZ_Redux.GetName()}"
+            final_model = rt.RooProdPdf(name, name, [polynomial_model,core_model])
+            
+            # add the information to relevant dictionaries
+            model_dict[subCat_name4coreModel] = final_model
+            subCat_dataHists[subCat_name4coreModel] = roo_histData
+            misc_dict[roo_datasetData.GetName()] = roo_datasetData
+            
+            # misc_dict.update(params_redux)
 
     print(f"misc_dict: {misc_dict}")
+    print(f"model_dict: {model_dict}")
     # Construct combined dataset
     combData = rt.RooDataSet(
         "combData",
@@ -119,28 +149,48 @@ if __name__ == "__main__":
         Import=subCat_dataHists,
     ) 
     # combData.Print("v")
-    # sample.Print("v")
+    sample.Print("v")
     # combHist = rt.RooDataHist("CombDataHist_Data",f"CombDataHist_Data", rt.RooArgSet(mass), combData)
     # combHist.Print("v")
 
     # Construct a simultaneous pdf using category sample as index: associate model
     simPdf = rt.RooSimultaneous("simPdf", "simultaneous pdf", model_dict, sample)
     _ = simPdf.fitTo(combData, rt.RooFit.Range(fit_range), EvalBackend="cpu", Save=True, )
+    fit_result = simPdf.fitTo(combData, rt.RooFit.Range(fit_range), EvalBackend="cpu", Save=True, )
 
-    # plot 
-    frame = mass.frame()
-    legend = rt.TLegend(0.65,0.55,0.9,0.7)
-    # apparently I have to plot invisible roo dataset for fit function plotting to work. Maybe this helps with normalization?
-    roo_datasetData.plotOn(frame, rt.RooFit.MarkerColor(0), rt.RooFit.LineColor(0) )
+    # save workspace for plotting later
+    fout = rt.TFile("./simultFitTestWorkspace.root","RECREATE")
+    wout = rt.RooWorkspace("workspace","workspace")
+    wout.Import(roo_datasetData);
     for model_name, model in model_dict.items():
-        color = subCatColorMap[model_name]
-        model.plotOn(frame, rt.RooFit.NormRange(fit_range), rt.RooFit.Range("full"), Name=model.GetName(), LineColor=color)
-        legend.AddEntry(frame.getObject(int(frame.numItems())-1),model.GetName(), "L")
+        wout.Import(model);
+    wout.Print();
+    wout.Write();
+    # # plot 
+    # frame = mass.frame()
+    # legend = rt.TLegend(0.65,0.55,0.9,0.7)
 
-    frame.Draw()
-    legend.Draw()        
-    canvas.Update()
-    canvas.Draw()
-    canvas.SaveAs(f"./quick_plots/simultaneousPlotTest.pdf")
+    # for core_model in core_models:
+        
+    #     canvas.Clear()
+    #     # apparently I have to plot invisible roo dataset for fit function plotting to work. Maybe this helps with normalization?
+    #     roo_datasetData.plotOn(frame, rt.RooFit.MarkerColor(0), rt.RooFit.LineColor(0) )
+
+    #     core_modelName = core_model.GetName()
+    #     for model_name, model in model_dict.items():
+    #         if core_modelName in model_name:
+    #             color = rt.kBlue
+    #             for subCat, col in subCatColorMap.items():
+    #                 if subCat in core_modelName:
+    #                     color = col
+    #             model.plotOn(frame, rt.RooFit.NormRange(fit_range), rt.RooFit.Range("full"), Name=model.GetName(), LineColor=color)
+    #             # model.plotOn(frame, rt.RooFit.NormRange(fit_range), rt.RooFit.Range("full"), Name=model.GetName())
+    #             legend.AddEntry(frame.getObject(int(frame.numItems())-1),model.GetName(), "L")
+    
+    #     frame.Draw()
+    #     legend.Draw()        
+    #     canvas.Update()
+    #     canvas.Draw()
+    #     canvas.SaveAs(f"./quick_plots/simultaneousPlotTest_{core_modelName}.pdf")
         
         
