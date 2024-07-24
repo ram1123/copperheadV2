352b41204a0266248cacd6ec2315983453551fb9
green-cabbage <hyeonseo_yun@hotmail.com>
2024-07-24 22:45:17 +0200

    good checkpoint the whole fit takes less than a second

--- quick_tests/simulFit_tutorial.py
+++ quick_tests/simulFit_tutorial.py
@@ -1,88 +1,120 @@
-import ROOT
 import time
- 
-# Create model for physics sample
-# -------------------------------------------------------------
-start = time.time()
-# Create observables
-mass = ROOT.RooRealVar("x", "x", -8, 8)
- 
-# Construct signal pdf
-mean = ROOT.RooRealVar("mean", "mean", 0, -8, 8)
-sigma = ROOT.RooRealVar("sigma", "sigma", 0.3, 0.1, 10)
-gx = ROOT.RooGaussian("gx", "gx", mass, mean, sigma)
- 
-# Construct background pdf
-a0 = ROOT.RooRealVar("a0", "a0", -0.1, -1, 1)
-a1 = ROOT.RooRealVar("a1", "a1", 0.004, -1, 1)
-px = ROOT.RooChebychev("px", "px", mass, [a0, a1])
- 
-# Construct composite pdf
-f = ROOT.RooRealVar("f", "f", 0.2, 0.0, 1.0)
-model = ROOT.RooAddPdf("model", "model", [gx, px], [f])
- 
-# Create model for control sample
-# --------------------------------------------------------------
- 
-# Construct signal pdf.
-# NOTE that sigma is shared with the signal sample model
-mean_ctl = ROOT.RooRealVar("mean_ctl", "mean_ctl", -3, -8, 8)
-gx_ctl = ROOT.RooGaussian("gx_ctl", "gx_ctl", mass, mean_ctl, sigma)
- 
-# Construct the background pdf
-a0_ctl = ROOT.RooRealVar("a0_ctl", "a0_ctl", -0.1, -1, 1)
-a1_ctl = ROOT.RooRealVar("a1_ctl", "a1_ctl", 0.5, -0.1, 1)
-px_ctl = ROOT.RooChebychev("px_ctl", "px_ctl", mass, [a0_ctl, a1_ctl])
- 
-# Construct the composite model
-f_ctl = ROOT.RooRealVar("f_ctl", "f_ctl", 0.5, 0.0, 1.0)
-model_ctl = ROOT.RooAddPdf("model_ctl", "model_ctl", [gx_ctl, px_ctl], [f_ctl])
- 
-# Generate events for both samples
-# ---------------------------------------------------------------
- 
-# Generate 1000 events in x and y from model
-data = model.generate({mass}, 1000)
-data_ctl = model_ctl.generate({mass}, 2000)
- 
-# Create index category and join samples
-# ---------------------------------------------------------------------------
- 
-# Define category to distinguish physics and control samples events
-sample = ROOT.RooCategory("sample", "sample")
-sample.defineType("physics")
-sample.defineType("control")
- 
-# Construct combined dataset in (x,sample)
-combData = ROOT.RooDataSet(
-    "combData",
-    "combined data",
-    {mass},
-    Index=sample,
-    Import={
-        "physics": data, 
-        "control": data_ctl
-    },
-)
- 
-# Construct a simultaneous pdf in (x, sample)
-# -----------------------------------------------------------------------------------
- 
-# Construct a simultaneous pdf using category sample as index: associate model
-# with the physics state and model_ctl with the control state
-simPdf = ROOT.RooSimultaneous("simPdf", "simultaneous pdf", 
-                              {
-                                  "physics": model, 
-                                   "control": model_ctl
-                              }, sample
-                             )
- 
-# Perform a simultaneous fit
-# ---------------------------------------------------
- 
-# Perform simultaneous fit of model to data and model_ctl to data_ctl
-fitResult = simPdf.fitTo(combData, PrintLevel=-1, Save=True)
-fitResult.Print()
+import numpy as np
+import pickle
+import awkward as ak
+import dask_awkward as dak
+from distributed import Client
+from omegaconf import OmegaConf
 
-end = time.time()
-print(f"runtime: {end-start} seconds")
+from typing import Tuple, List, Dict
+import ROOT as rt
+
+if __name__ == "__main__":
+    load_path = "./processed_events_data.parquet"
+    processed_eventsData = ak.from_parquet(load_path)
+    print("events loaded!")
+    
+    # Create model for physics sample
+    # -------------------------------------------------------------
+    # Create observables
+    mass_name = "mh_ggh"
+    mass = rt.RooRealVar(mass_name, mass_name, -8, 8)
+    nbins = 800
+    mass.setBins(nbins)
+    
+    # Construct signal pdf
+    mean = rt.RooRealVar("mean", "mean", 0, -8, 8)
+    sigma = rt.RooRealVar("sigma", "sigma", 0.3, 0.1, 10)
+    gx = rt.RooGaussian("gx", "gx", mass, mean, sigma)
+     
+    # Construct background pdf
+    a0 = rt.RooRealVar("a0", "a0", -0.1, -1, 1)
+    a1 = rt.RooRealVar("a1", "a1", 0.004, -1, 1)
+    px = rt.RooChebychev("px", "px", mass, [a0, a1])
+     
+    # Construct composite pdf
+    f = rt.RooRealVar("f", "f", 0.2, 0.0, 1.0)
+    model = rt.RooAddPdf("model", "model", [gx, px], [f])
+     
+    # Create model for control sample
+    # --------------------------------------------------------------
+     
+    # Construct signal pdf.
+    # NOTE that sigma is shared with the signal sample model
+    mean_ctl = rt.RooRealVar("mean_ctl", "mean_ctl", -3, -8, 8)
+    gx_ctl = rt.RooGaussian("gx_ctl", "gx_ctl", mass, mean_ctl, sigma)
+     
+    # Construct the background pdf
+    a0_ctl = rt.RooRealVar("a0_ctl", "a0_ctl", -0.1, -1, 1)
+    a1_ctl = rt.RooRealVar("a1_ctl", "a1_ctl", 0.5, -0.1, 1)
+    px_ctl = rt.RooChebychev("px_ctl", "px_ctl", mass, [a0_ctl, a1_ctl])
+     
+    # Construct the composite model
+    f_ctl = rt.RooRealVar("f_ctl", "f_ctl", 0.5, 0.0, 1.0)
+    model_ctl = rt.RooAddPdf("model_ctl", "model_ctl", [gx_ctl, px_ctl], [f_ctl])
+     
+    # Generate events for both samples
+    # ---------------------------------------------------------------
+     
+    # do for cat idx 0
+    subCat_filter = (processed_eventsData["subCategory_idx"] == 0)
+    subCat_mass_arr = processed_eventsData.dimuon_mass[subCat_filter]
+    subCat_mass_arr  = ak.to_numpy(subCat_mass_arr) # convert to numpy for rt.RooDataSet
+    roo_datasetData_subCat0 = rt.RooDataSet.from_numpy({mass_name: subCat_mass_arr}, [mass])
+    data = roo_datasetData_subCat0
+
+    subCat_filter = (processed_eventsData["subCategory_idx"] == 1)
+    subCat_mass_arr = processed_eventsData.dimuon_mass[subCat_filter]
+    subCat_mass_arr  = ak.to_numpy(subCat_mass_arr) # convert to numpy for rt.RooDataSet
+    roo_datasetData_subCat1 = rt.RooDataSet.from_numpy({mass_name: subCat_mass_arr}, [mass])
+    data_ctl = roo_datasetData_subCat1
+
+    # # Generate 1000 events in x and y from model
+    # data = model.generate({x}, 1000)
+    # data_ctl = model_ctl.generate({x}, 2000)
+     
+    # Create index category and join samples
+    # ---------------------------------------------------------------------------
+     
+    # Define category to distinguish physics and control samples events
+    sample = rt.RooCategory("sample", "sample")
+    sample.defineType("physics")
+    sample.defineType("control")
+     
+    # Construct combined dataset in (x,sample)
+    combData = rt.RooDataSet(
+        "combData",
+        "combined data",
+        {mass},
+        Index=sample,
+        Import={
+            "physics": data, 
+            "control": data_ctl
+        },
+    )
+     
+    # Construct a simultaneous pdf in (x, sample)
+    # -----------------------------------------------------------------------------------
+     
+    # Construct a simultaneous pdf using category sample as index: associate model
+    # with the physics state and model_ctl with the control state
+    simPdf = rt.RooSimultaneous("simPdf", "simultaneous pdf", 
+                                  {
+                                      "physics": model, 
+                                       "control": model_ctl
+                                  }, sample
+                                 )
+     
+    # Perform a simultaneous fit
+    # ---------------------------------------------------
+     
+    # Perform simultaneous fit of model to data and model_ctl to data_ctl
+    start = time.time()
+    
+    fitResult = simPdf.fitTo(combData, PrintLevel=-1, Save=True)
+    end = time.time()
+    
+    fitResult.Print()
+    print(f"runtime: {end-start} seconds")
+
+
