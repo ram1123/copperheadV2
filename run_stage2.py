d20e1d9d3a2851a966a031ddcefebc75844f605e
green-cabbage <hyeonseo_yun@hotmail.com>
2024-07-29 17:59:33 +0200

    more refactoring

--- run_stage2.py
+++ run_stage2.py
@@ -39,6 +39,16 @@ if __name__ == "__main__":
     action="store",
     help="string value of year we are calculating",
     )
+    parser.add_argument(
+    "-samp",
+    "--samples",
+    dest="samples",
+    default=[],
+    nargs="*",
+    type=str,
+    action="store",
+    help="list of samples to process for stage2. Current valid inputs are data, signal and DY",
+    )
     args = parser.parse_args()
     # check for valid arguments
     if args.load_path == None:
@@ -46,6 +56,9 @@ if __name__ == "__main__":
         raise ValueError
     if args.save_path == None:
         print("save path is not specified!")
+        raise ValueError
+    if len(args.samples) == 0:
+        print("samples list is zero!")
         raise ValueError
     
     load_path = f"{args.load_path}/{args.year}/f1_0"
@@ -56,109 +69,122 @@ if __name__ == "__main__":
     # full_load_path = load_path+f"/data_*/*/*.parquet"
     # full_load_path = load_path+f"/data_A/*/*.parquet"
     # full_load_path = load_path+f"/ggh_powheg/*/*.parquet"
-    full_load_path = load_path+f"/dy_M-100To200/*/*.parquet"
+    # full_load_path = load_path+f"/dy_M-100To200/*/*.parquet"
 
-    client =  Client(n_workers=31,  threads_per_worker=1, processes=True, memory_limit='4 GiB') 
-    events = dak.from_parquet(full_load_path)
-
-    # load and obtain MVA outputs
-    events["dimuon_dEta"] = np.abs(events.mu1_pt - events.mu2_pt)
-    events["dimuon_pt_log"] = np.log(events.dimuon_pt)
-    events["jj_mass_log"] = np.log(events.jj_mass)
-    events["ll_zstar_log"] = np.log(events.ll_zstar)
-    events["mu1_pt_over_mass"] = events.mu1_pt / events.dimuon_mass
-    events["mu2_pt_over_mass"] = events.mu2_pt / events.dimuon_mass
-
-    train_feat_dict = {
-        "BDTperyear_2018" : [
-            'dimuon_cos_theta_cs', 'dimuon_dEta', 'dimuon_dPhi', 'dimuon_dR', 'dimuon_eta', 'dimuon_phi', 'dimuon_phi_cs', 'dimuon_pt', 
-            'dimuon_pt_log', 'jet1_eta', 'jet1_phi', 'jet1_pt', 'jet1_qgl', 'jet2_eta', 'jet2_phi', 
-            'jet2_pt', 'jet2_qgl', 'jj_dEta', 'jj_dPhi', 'jj_eta', 'jj_mass', 'jj_mass_log', 
-            'jj_phi', 'jj_pt', 'll_zstar_log', 'mmj1_dEta', 'mmj1_dPhi', 
-            'mmj_min_dEta', 'mmj_min_dPhi', 'mmjj_eta', 'mmjj_mass', 'mmjj_phi', 'mmjj_pt', 'mu1_eta', 'mu1_iso', 
-            'mu1_phi', 'mu1_pt_over_mass', 'mu2_eta', 'mu2_iso', 'mu2_phi', 'mu2_pt_over_mass', 'zeppenfeld'
-        ],
-        "phifixedBDT_2018" : [
-                'dimuon_cos_theta_cs', 'dimuon_eta', 'dimuon_phi_cs', 'dimuon_pt', 'jet1_eta', 'jet1_pt', 'jet2_eta', 'jet2_pt', 'jj_dEta', 'jj_dPhi', 'jj_mass', 'mmj1_dEta', 'mmj1_dPhi',  'mmj_min_dEta', 'mmj_min_dPhi', 'mu1_eta', 'mu1_pt_over_mass', 'mu2_eta', 'mu2_pt_over_mass', 'zeppenfeld' #, 'njets'
-        ], # AN 19-124
+    for sample in args.samples:
+        if sample.lower() == "data":
+            full_load_path = load_path+f"/data_*/*/*.parquet"
+        elif sample.lower() == "signal":
+            full_load_path = load_path+f"/ggh_powheg/*/*.parquet"
+        elif sample.lower() == "dy":
+            full_load_path = load_path+f"/dy_M-100To200/*/*.parquet"
+        else:
+            print(f"unsupported sample!")
+            raise ValueError
+            
+        print(f"full_load_path: {full_load_path}")
+    
+        client =  Client(n_workers=31,  threads_per_worker=1, processes=True, memory_limit='4 GiB') 
+        events = dak.from_parquet(full_load_path)
+    
+        # load and obtain MVA outputs
+        events["dimuon_dEta"] = np.abs(events.mu1_pt - events.mu2_pt)
+        events["dimuon_pt_log"] = np.log(events.dimuon_pt)
+        events["jj_mass_log"] = np.log(events.jj_mass)
+        events["ll_zstar_log"] = np.log(events.ll_zstar)
+        events["mu1_pt_over_mass"] = events.mu1_pt / events.dimuon_mass
+        events["mu2_pt_over_mass"] = events.mu2_pt / events.dimuon_mass
+    
+        train_feat_dict = {
+            "BDTperyear_2018" : [
+                'dimuon_cos_theta_cs', 'dimuon_dEta', 'dimuon_dPhi', 'dimuon_dR', 'dimuon_eta', 'dimuon_phi', 'dimuon_phi_cs', 'dimuon_pt', 
+                'dimuon_pt_log', 'jet1_eta', 'jet1_phi', 'jet1_pt', 'jet1_qgl', 'jet2_eta', 'jet2_phi', 
+                'jet2_pt', 'jet2_qgl', 'jj_dEta', 'jj_dPhi', 'jj_eta', 'jj_mass', 'jj_mass_log', 
+                'jj_phi', 'jj_pt', 'll_zstar_log', 'mmj1_dEta', 'mmj1_dPhi', 
+                'mmj_min_dEta', 'mmj_min_dPhi', 'mmjj_eta', 'mmjj_mass', 'mmjj_phi', 'mmjj_pt', 'mu1_eta', 'mu1_iso', 
+                'mu1_phi', 'mu1_pt_over_mass', 'mu2_eta', 'mu2_iso', 'mu2_phi', 'mu2_pt_over_mass', 'zeppenfeld'
+            ],
+            "phifixedBDT_2018" : [
+                    'dimuon_cos_theta_cs', 'dimuon_eta', 'dimuon_phi_cs', 'dimuon_pt', 'jet1_eta', 'jet1_pt', 'jet2_eta', 'jet2_pt', 'jj_dEta', 'jj_dPhi', 'jj_mass', 'mmj1_dEta', 'mmj1_dPhi',  'mmj_min_dEta', 'mmj_min_dPhi', 'mu1_eta', 'mu1_pt_over_mass', 'mu2_eta', 'mu2_pt_over_mass', 'zeppenfeld' #, 'njets'
+            ], # AN 19-124
+            
+        }
         
-    }
+        model_name = "phifixedBDT_2018"
+        # model_name = "BDTperyear_2018"
     
-    model_name = "phifixedBDT_2018"
-    # model_name = "BDTperyear_2018"
-
-    training_features = train_feat_dict[model_name]
-    print(f"len(training_features): {len(training_features)}")
-
-    # load training features from the ak.Record
-    for training_feature in training_features:
-        if training_feature not in events.fields:
-            print(f"mssing feature: {training_feature}")
+        training_features = train_feat_dict[model_name]
+        print(f"len(training_features): {len(training_features)}")
     
-    fields2load = training_features + ["h_peak", "h_sidebands", "dimuon_mass", "wgt_nominal_total", "mmj2_dEta", "mmj2_dPhi"]
-    events = events[fields2load]
-    # load data to memory using compute()
-    events = ak.zip({
-        field : events[field] for field in events.fields
-    }).compute()
-
-
-    parameters = {
-    "models_path" : "/depot/cms/hmm/vscheure/data/trained_models/"
-    }
+        # load training features from the ak.Record
+        for training_feature in training_features:
+            if training_feature not in events.fields:
+                print(f"mssing feature: {training_feature}")
+        
+        fields2load = training_features + ["h_peak", "h_sidebands", "dimuon_mass", "wgt_nominal_total", "mmj2_dEta", "mmj2_dPhi"]
+        events = events[fields2load]
+        # load data to memory using compute()
+        events = ak.zip({
+            field : events[field] for field in events.fields
+        }).compute()
     
     
-    processed_events = evaluate_bdt(events, "nominal", model_name, training_features, parameters) # this also only filters in h_peak and h_sidebands
-
-    # load BDT score edges for subcategory divison
-    BDTedges_load_path = "./configs/MVA/ggH/BDT_edges.yaml"
-    edges = OmegaConf.load(BDTedges_load_path)
-    year = "2018"
-    edges = np.array(edges[year])
-    print(f"subCat BDT edges: {edges}")
-
-    # Calculate the subCategory index 
-    BDT_score = processed_events["BDT_score"]
-    n_edges = len(edges)
-    BDT_score_repeat = ak.concatenate([BDT_score[:,np.newaxis] for i in range(n_edges)], axis=1)
-    # BDT_score_repeat
-    n_rows = len(BDT_score_repeat)
-    edges_repeat = np.repeat(edges[np.newaxis,:],n_rows,axis=0)
-    # edges_repeat.shape
-    edge_idx = ak.sum( (BDT_score_repeat >= edges_repeat), axis=1)
-    subCat_idx =  edge_idx - 1 # sub category index starts at zero
-    processed_events["subCategory_idx"] = subCat_idx
-
-    # filter in only the variables you need to do stage3
-    fields2save = [
-        "dimuon_mass",
-        "BDT_score",
-        "subCategory_idx",
-        "wgt_nominal_total",
-    ]
-    processed_events = ak.zip({
-        field : processed_events[field] for field in fields2save
-    })
-    # define save path and save
-    # save_path = "/work/users/yun79/stage2_output/ggH/test"
-    save_path = args.save_path
-        # make save path if it doesn't exist
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
-    if "data" in full_load_path:
-        save_filename = save_path+"/processed_events_data.parquet"  
-    elif "ggh_powheg" in full_load_path: # else, ggh powheg
-        save_filename = save_path+"/processed_events_signalMC.parquet" 
-    elif "dy_M-100To200" in full_load_path:
-        save_filename = save_path+"/processed_events_dyMC.parquet" 
-    print(f"save_filename: {save_filename}")
-
-    # delete the file if there's already same save_filename
-    try:
-        os.remove(save_filename)
-    except:
-        pass
-    ak.to_parquet(processed_events, save_filename)
+        parameters = {
+        "models_path" : "/depot/cms/hmm/vscheure/data/trained_models/"
+        }
+        
+        
+        processed_events = evaluate_bdt(events, "nominal", model_name, training_features, parameters) # this also only filters in h_peak and h_sidebands
+    
+        # load BDT score edges for subcategory divison
+        BDTedges_load_path = "./configs/MVA/ggH/BDT_edges.yaml"
+        edges = OmegaConf.load(BDTedges_load_path)
+        year = "2018"
+        edges = np.array(edges[year])
+        print(f"subCat BDT edges: {edges}")
+    
+        # Calculate the subCategory index 
+        BDT_score = processed_events["BDT_score"]
+        n_edges = len(edges)
+        BDT_score_repeat = ak.concatenate([BDT_score[:,np.newaxis] for i in range(n_edges)], axis=1)
+        # BDT_score_repeat
+        n_rows = len(BDT_score_repeat)
+        edges_repeat = np.repeat(edges[np.newaxis,:],n_rows,axis=0)
+        # edges_repeat.shape
+        edge_idx = ak.sum( (BDT_score_repeat >= edges_repeat), axis=1)
+        subCat_idx =  edge_idx - 1 # sub category index starts at zero
+        processed_events["subCategory_idx"] = subCat_idx
+    
+        # filter in only the variables you need to do stage3
+        fields2save = [
+            "dimuon_mass",
+            "BDT_score",
+            "subCategory_idx",
+            "wgt_nominal_total",
+        ]
+        processed_events = ak.zip({
+            field : processed_events[field] for field in fields2save
+        })
+        # define save path and save
+        # save_path = "/work/users/yun79/stage2_output/ggH/test"
+        save_path = args.save_path
+            # make save path if it doesn't exist
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+        if "data" in full_load_path:
+            save_filename = save_path+"/processed_events_data.parquet"  
+        elif "ggh_powheg" in full_load_path: # else, ggh powheg
+            save_filename = save_path+"/processed_events_signalMC.parquet" 
+        elif "dy_M-100To200" in full_load_path:
+            save_filename = save_path+"/processed_events_dyMC.parquet" 
+        print(f"save_filename: {save_filename}")
+    
+        # delete the file if there's already same save_filename
+        try:
+            os.remove(save_filename)
+        except:
+            pass
+        ak.to_parquet(processed_events, save_filename)
 
 
     
