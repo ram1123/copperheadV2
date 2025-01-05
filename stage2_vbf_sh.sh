5af648f8b49393b26bb20fc79142c331dea9944e
green-cabbage <hyeonseo_yun@hotmail.com>
2025-01-05 03:45:16 +0100

    adding stage3 script and updating stage2 one

--- stage2_vbf_sh.sh
+++ stage2_vbf_sh.sh
@@ -4,5 +4,10 @@
 model_label="Dec31_stage2_test"
 stage1_lable="V2_Dec22_HEMVetoOnZptOn_RerecoBtagSF_XS_Rereco_BtagWPsFixed"
 year="2018"
-python run_stage2_vbf.py --model_label $model_label --run_label $stage1_lable -y $year --use_gateway 
-# python run_stage2_vbf.py --model_label $model_label --run_label $stage1_lable -y $year
+# data_samples=""
+# bkg_samples=""
+data_samples="A B C D"
+bkg_samples="DY TT"
+sig_samples="VBF GGH"
+# python run_stage2_vbf.py --model_label $model_label --run_label $stage1_lable -y $year -data $data_samples -bkg $bkg_samples -sig $sig_samples --use_gateway 
+python run_stage2_vbf.py --model_label $model_label --run_label $stage1_lable -y $year -data $data_samples -bkg $bkg_samples -sig $sig_samples
