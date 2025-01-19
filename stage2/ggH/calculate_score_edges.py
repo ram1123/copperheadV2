c1ed42081ce0efaf7a86277cb9af2fe5e69d51e1
green-cabbage <hyeonseo_yun@hotmail.com>
2025-01-19 17:13:40 +0100

    add calculating new bdt score edges with a given yield targets

---
+++ stage2/ggH/calculate_score_edges.py
@@ -0,0 +1,89 @@
+import dask_awkward as dak 
+import awkward as ak
+import numpy as np
+import argparse
+from omegaconf import OmegaConf
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+    "-y",
+    "--year",
+    dest="year",
+    default="2018",
+    action="store",
+    help="Year to process (2016preVFP, 2016postVFP, 2017 or 2018)",
+    )
+    parser.add_argument(
+    "-load",
+    "--load_path",
+    dest="load_path",
+    default="",
+    action="store",
+    help="path were stage2 output is saved",
+    )
+    sysargs = parser.parse_args()
+
+
+    # full_load_path = "/depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/ggh/stage2_output/ggh/2018/processed_events_sig*.parquet"
+    # extract only the signal samples (VBF and ggH)
+    full_load_path = f"{sysargs.load_path}/{sysargs.year}/processed_events_sig*.parquet"
+    # full_load_path = f"{sysargs.load_path}/ggh/{sysargs.year}/processed_events_sig*.parquet"
+    events = dak.from_parquet(full_load_path)
+    
+    signal_score = ak.to_numpy(events.BDT_score.compute())
+    signal_wgt = ak.to_numpy(events.wgt_nominal.compute())
+    signal_wgt = signal_wgt /np.sum(signal_wgt) # normalize wgt
+    target_yields = [0.3, 0.35, 0.15, 0.15, 0.05]
+    print(sum(target_yields))
+    target_yields_cum_sum = np.cumsum(np.array(target_yields))
+    # sort data
+    sorted_indices = np.argsort(signal_score)
+    signal_score = signal_score[sorted_indices]
+    signal_wgt =signal_wgt[sorted_indices]
+    print(f"target_yields_cum_sum: {target_yields_cum_sum}")
+
+    # Compute cumulative weights
+    cumulative_weights = np.cumsum(signal_wgt)
+    # Find bin edges
+    sorted_data = signal_score
+    bin_edges = [0.0]  # Start with the minimum value
+    current_yield = 0
+    target_index = 0
+    
+    for i, cum_weight in enumerate(cumulative_weights):
+        current_yield = cum_weight 
+        if current_yield >= target_yields_cum_sum[target_index]:
+            bin_edges.append(sorted_data[i])
+            target_index += 1
+            if target_index >= len(target_yields_cum_sum):
+                break
+    
+    
+    
+    bin_edges[-1] = 1.1 # always switch the last bin edge to maximum value and a bit
+    print("Bin edges:", bin_edges)
+    hist, _ = np.histogram(signal_score, bins=bin_edges, weights=signal_wgt)
+    # sanity check
+    print(f"original target_yields: {target_yields}")
+    print(f"binnning histogram distribution: {hist}")
+    print(f"np.sum(hist): {np.sum(hist)}")
+    signal_subcat_idx = np.digitize(signal_score, bin_edges) -1 # digitize starts at one, not zero
+    print(f"signal_subcat_idx: {signal_subcat_idx}")
+    print(f"np.max(signal_score): {np.max(signal_score)}")
+
+
+    # save the new bin edges
+    config_path = f"/work/users/yun79/valerie/fork/copperheadV2/configs/MVA/ggH/BDT_edges.yaml"
+    # Load the config file
+    config = OmegaConf.load(config_path)
+    print(f"old config: {config}")
+    bin_edges = [float(value) for value in bin_edges] # need to convert to 32-bit b4 writing to omegaconf
+    config[sysargs.year] = bin_edges 
+    print(f"new config: {config}")
+    # Overwrite the yaml file
+    OmegaConf.save(config, config_path)
+    
--- stage2_sh.sh
+++ stage2_sh.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
-year="2018"
+# run stage2 twice. First to generate BDT scores (we assume that an appropriate BDT is already trained, then generate score bin edges once more, then finally run stage2 again to save both bdt scores and ggH sub-category index
+
 # year="2017"
 # year="2016postVFP"
 # year="2016preVFP"
 
 
 sample_l="data ggh vbf" 
-stage2_load_path="/depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan16_JecDefault_plotEveryonesZptWgt/"
-stage2_save_path="/depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan16_JecDefault_plotEveryonesZptWgt/ggh/stage2_output" 
+stage2_load_path="/depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output"
+
 category="ggh"
+stage2_save_path="/depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/$category/stage2_output" # I like to specify the category in the save path
+
 model="V2_UL_Jan18_2025"
+
+
+# year="2018"
+# python run_stage2.py -load $stage2_load_path -save $stage2_save_path --samples $sample_l -cat $category --fraction 1.0 --year $year --model_name $model
+# python stage2/ggH/calculate_score_edges.py -load $stage2_save_path --year $year 
+# python run_stage2.py -load $stage2_load_path -save $stage2_save_path --samples $sample_l -cat $category --fraction 1.0 --year $year --model_name $model
+
+year="2017"
+python run_stage2.py -load $stage2_load_path -save $stage2_save_path --samples $sample_l -cat $category --fraction 1.0 --year $year --model_name $model
+python stage2/ggH/calculate_score_edges.py -load $stage2_save_path --year $year 
+python run_stage2.py -load $stage2_load_path -save $stage2_save_path --samples $sample_l -cat $category --fraction 1.0 --year $year --model_name $model
+
+
+year="2016preVFP"
+python run_stage2.py -load $stage2_load_path -save $stage2_save_path --samples $sample_l -cat $category --fraction 1.0 --year $year --model_name $model
+python stage2/ggH/calculate_score_edges.py -load $stage2_save_path --year $year 
 python run_stage2.py -load $stage2_load_path -save $stage2_save_path --samples $sample_l -cat $category --fraction 1.0 --year $year --model_name $model
