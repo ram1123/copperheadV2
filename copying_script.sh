6ba1acae928bae1068c55a5a24a215c248e1869b
green-cabbage <hyeonseo_yun@hotmail.com>
2025-01-27 02:58:11 +0100

    update copying script

---
+++ copying_script.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+year="2017"
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/ttjets*  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/*top  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/ggh*  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/vbf*  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/ww*  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/wz*  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/zz  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/ewk*  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
+
+
+year="2016postVFP"
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/ttjets*  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/*top  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/ggh*  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/vbf*  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/ww*  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/wz*  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/zz  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/ewk*  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
+
+
+year="2016preVFP"
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/ttjets*  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/*top  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/ggh*  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/vbf*  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/ww*  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/wz*  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/zz  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
+
+cp -r /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan17_JecDefault_valerieZpt/stage1_output/$year/f1_0/ewk*  /depot/cms/users/yun79/hmm/copperheadV1clean/V2_Jan09_ForZptReWgt//stage1_output/$year/f1_0/
