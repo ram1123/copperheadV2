8964300ef9da971666ae83ec1b5906fa2ea5955f
green-cabbage <hyeonseo_yun@hotmail.com>
2025-01-30 02:04:51 +0100

    adding inv jet1ptCut to ggH category

--- configs/categories/category_cuts.py
+++ configs/categories/category_cuts.py
@@ -6,40 +6,12 @@ wrapper
 import awkward as ak
 
 
-# class CustomCat_VBF:
-#     name = "Cat_VBF"
-#     def __init__(self):
-#         pass
-#     def filterCategory(events): # apparently self is not needed
-#         production_cat_cut = ak.fill_none(events.vbf_cut, value=False) # this require jj_mass > 400 AND jj_dEta > 2.5
-#         btag_cut =(events.nBtagLoose >= 2) | (events.nBtagMedium >= 1)
-#         vbfCat_selection = (
-#             production_cat_cut  
-#             & ~btag_cut # btag cut is for VH and ttH categories
-#         )
-#         return vbfCat_selection
-
-# class CustomCat_ggH:
-#     name = "Cat_ggH"
-#     def __init__(self):
-#         pass
-#     def filterCategory(events): # apparently self is not needed
-#         vbf_cut = ak.fill_none(events.vbf_cut, value=False) # this require jj_mass > 400 AND jj_dEta > 2.5
-#         production_cat_cut = ~vbf_cut
-#         btag_cut =(events.nBtagLoose >= 2) | (events.nBtagMedium >= 1)
-#         ggHCat_selection = (
-#             production_cat_cut  
-#             & ~btag_cut # btag cut is for VH and ttH categories
-#         )
-#         return ggHCat_selection
-
-
 class CustomInvBtagCut:
     name = "InvBtagCut"
     def __init__(self):
         pass
     def filterCategory(events): # apparently self is not needed
-        btag_cut = ak.fill_none(events.nBtagLoose >= 2, value=False) | ak.fill_none(events.nBtagMedium >= 1, value=False)
+        btag_cut = ak.fill_none(events.nBtagLoose_nominal >= 2, value=False) | ak.fill_none(events.nBtagMedium_nominal >= 1, value=False)
         return ~btag_cut
 
 class CustomVbfCut: # specified in line 827 of AN-19-124
@@ -48,7 +20,7 @@ class CustomVbfCut: # specified in line 827 of AN-19-124
         pass
     def filterCategory(events): # apparently self is not needed
         # vbf_cut = ak.fill_none(events.vbf_cut, value=False) # this require jj_mass > 400 AND jj_dEta > 2.5
-        vbf_cut = (events.jj_mass > 400) & (events.jj_dEta > 2.5) 
+        vbf_cut = (events.jj_mass_nominal > 400) & (events.jj_dEta_nominal > 2.5) 
         vbf_cut = ak.fill_none(vbf_cut, value=False)
         return vbf_cut
 
@@ -57,7 +29,7 @@ class CustomJet1PtCut: # specified in line 827 of AN-19-124
     def __init__(self):
         pass
     def filterCategory(events): # apparently self is not needed
-        jet1_ptCut = ak.fill_none(events.jet1_pt > 35, value=False) 
+        jet1_ptCut = ak.fill_none(events.jet1_pt_nominal > 35, value=False) 
         return jet1_ptCut
 
 class CustomInvVbfCut: # specified in line 830 of AN-19-124
@@ -66,6 +38,14 @@ class CustomInvVbfCut: # specified in line 830 of AN-19-124
         pass
     def filterCategory(events): # apparently self is not needed
         # vbf_cut = ak.fill_none(events.vbf_cut, value=False) # this require jj_mass > 400 AND jj_dEta > 2.5
-        vbf_cut = (events.jj_mass > 400) & (events.jj_dEta > 2.5) 
+        vbf_cut = (events.jj_mass_nominal > 400) & (events.jj_dEta_nominal > 2.5) 
         vbf_cut = ak.fill_none(vbf_cut, value=False)
         return ~vbf_cut
+
+class CustomInvJet1PtCut: # specified in line 827 of AN-19-124
+    name = "InvJet1PtCut"
+    def __init__(self):
+        pass
+    def filterCategory(events): # apparently self is not needed
+        jet1_ptCut = ak.fill_none(events.jet1_pt_nominal > 35, value=False) 
+        return ~jet1_ptCut
