5729d07689412373bce6df809e44da8e28720d93
ram1123 <ramkrishna.sharma71@gmail.com>
2025-02-04 21:45:41 +0100

    Added .gitignore file and new README

---
+++ README_Feb2025.md
@@ -0,0 +1,14 @@
+# 🐍 Copperhead V2, - Columnar Parallel Pythonic framEwork for Run3 H&rarr;µµ Decay search
+
+## setup
+
+```bash
+git clone https://github.com/green-cabbage/copperheadV2.git
+cd copperheadV2
+conda activate /depot/cms/kernels/coffea_latest
+source setup_proxy.sh
+# Run first two column of DaskGatewaySLURM.ipynb to start the DASK.
+```
+
+## Step - 1: Skim, $Z_{p_T}$ correction
+
--- setup_proxy.sh
+++ setup_proxy.sh
@@ -1,11 +1,6 @@
 conda activate /depot/cms/kernels/root632
-voms-proxy-init -voms cms -rfc -valid 192:00
-export RUCIO_ACCOUNT=hyeonseo
-export VOMS_PATH=$(echo $(voms-proxy-info | grep path) | sed 's/path.*: //')
-export VOMS_USERID=$(echo $(voms-proxy-info | grep path) | sed 's/.*p_u//')
-export VOMS_TRG=/depot/cms/users/$USER/x509up_u$VOMS_USERID
-cp $VOMS_PATH $VOMS_TRG
-echo "Your proxy is copied here: "$VOMS_TRG
-export X509_USER_PROXY=$VOMS_TRG
+voms-proxy-init -voms cms -rfc -valid 192:00 --out /depot/cms/users/$USER/voms_proxy.txt
+export X509_USER_PROXY=/depot/cms/users/$USER/voms_proxy.txt
+export RUCIO_ACCOUNT=$USER
 export WORKDIR=$PWD
 export XRD_REQUESTTIMEOUT=2400
