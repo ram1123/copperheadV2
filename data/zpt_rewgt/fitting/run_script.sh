475aed1944f64c56b72d8dd9ab417b602df28b25
green-cabbage <hyeonseo_yun@hotmail.com>
2025-03-27 15:51:01 +0100

    updating zpt wgt script

--- data/zpt_rewgt/fitting/run_script.sh
+++ data/zpt_rewgt/fitting/run_script.sh
@@ -5,9 +5,10 @@
 # first rederive the SF root files for fitting
 year="2018"
 # year="all"
-label="UpdatedDY_100_200_CrossSection_24Feb_jetpuidOff"
+# label="UpdatedDY_100_200_CrossSection_24Feb_jetpuidOff"
+label="DYMiNNLO_jetpuidOff_newZptWgt25Mar2025"
 
-# python save_SF_rootFiles.py -l ${label} -y ${year}
+python save_SF_rootFiles.py -l ${label} -y ${year}
 
 # do F test
 # python do_f_test.py
