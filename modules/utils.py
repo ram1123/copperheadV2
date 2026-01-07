22c074918f425bea7b721cec37e33b6fca0b2afc
green-cabbage <hyeonseo_yun@hotmail.com>
2026-01-08 00:03:51 +0100

    update my stage3 changes to nanoV15 branch. Addresses ram1123/copperheadV2#92 and  ram1123/copperheadV2#93

--- modules/utils.py
+++ modules/utils.py
@@ -4,7 +4,11 @@ from rich.console import Console
 from typing import Optional
 import os
 import sys
+import numpy as np
 import awkward as ak
+import ROOT
+import ROOT as rt
+import matplotlib.pyplot as plt
 
 LOGGER_NAME = "CopperHead"
 NO_GIT_INFO_AVAILABLE = "No git info available"
@@ -128,3 +132,186 @@ def filterRegion(events, region="h-peak"):
 
     events = events[region]
     return events
+
+def convertVectorType4D(vector, vector_name):
+    new_vector = ak.zip(
+        {
+            "pt": vector.pt,
+            "eta": vector.eta,
+            "phi": vector.phi,
+            "mass": vector.mass,
+            "charge": vector.charge,
+        },
+        with_name=vector_name,
+        behavior=vector.behavior,
+    )
+
+def print_workspace_vars(ws):
+    for var in ws.allVars():
+        name = var.GetName()
+        val  = var.getVal()
+        vmin = var.getMin()
+        vmax = var.getMax()
+        is_const = var.isConstant()
+        print(
+            f"{name:20s} = {val:10.5f} "
+            f"range = [{vmin:10.5f}, {vmax:10.5f}] "
+            f"{'(const)' if is_const else ''}"
+        )
+
+
+def freeze_all_vars(w, make_exception=[]):
+    for v in w.allVars():
+        do_freeze = True
+        name = v.GetName()
+        # print(f"name: {name}")
+        for exception_name in make_exception:
+            if (exception_name in name) or (exception_name == name): # skip
+                do_freeze = False
+                continue
+        if do_freeze:
+            v.setConstant(True)
+
+def getNewRangeHist(hist2copy: rt.TH1D, new_hist_name: str, xlow_new: float, xhigh_new: float):
+    """
+    """
+    # get binwise content
+    # Initialize result dictionary
+    bin_dict = {"xlow": [], "xhigh": [], "content": []}
+    
+    # Fill dictionary with bin info
+    hist = hist2copy
+    for i in range(1, hist.GetNbinsX() + 1):
+        xlow = hist.GetBinLowEdge(i)
+        xhigh = xlow + hist.GetBinWidth(i)
+        content = hist.GetBinContent(i)
+        if (xlow >= xlow_new) and (xhigh <= xhigh_new):
+            bin_dict["xlow"].append(xlow)
+            bin_dict["xhigh"].append(xhigh)
+            bin_dict["content"].append(content)
+        
+    # print(bin_dict)
+    new_nbins = len(bin_dict["content"])
+
+    new_hist = rt.TH1D(new_hist_name, new_hist_name, new_nbins, xlow_new, xhigh_new)
+    for i in range(1, new_hist.GetNbinsX() + 1):
+        content = bin_dict["content"][i-1]
+        new_hist.SetBinContent(i, content)
+
+    # # sanity check
+    # for i in range(1, new_hist.GetNbinsX() + 1):
+    #     xlow = new_hist.GetBinLowEdge(i)
+    #     xhigh = xlow + new_hist.GetBinWidth(i)
+    #     content = new_hist.GetBinContent(i)
+    #     print(f"{new_hist_name} bin{i} {(xlow, xhigh, content)}")
+    # print(f"new_nbins: {new_nbins}")
+    # raise ValueError
+    return new_hist
+
+def getGOF_KS(x: rt.RooRealVar, data: rt.RooDataHist, pdf: rt.RooAbsPdf, cat_name:str, save_path:str):
+    """
+    Get KS value for specific value
+    """
+    # raise ValueError
+    nbins = x.getBins()
+    var_name = x.GetName()
+    # # Generate toy dataset
+    # data = pdf.generate(rt.RooArgSet(x), 1000)
+    hist_data_orig = data.createHistogram(var_name).Clone("clone")# clone it just in case
+    # Create a histogram of the PDF
+    hist_pdf_orig = pdf.createHistogram(var_name, nbins)
+
+
+    plot_line_width = 0.5
+    # -------------------------------------------
+    # Sanity check: plot the pdf and cdf of signal region PDF
+    # -------------------------------------------
+    # data_counts = np.array([hist_data_orig.GetBinContent(i) for i in range(1, hist_data_orig.GetNbinsX()+1)])
+    pdf_counts = np.array([hist_pdf_orig.GetBinContent(i) for i in range(1, hist_pdf_orig.GetNbinsX()+1)])
+    # print(f"data_counts: {data_counts}")
+    # print(f"pdf_counts: {pdf_counts}")
+    pdf_cdf = np.cumsum(pdf_counts) / np.sum(pdf_counts)
+    
+    bin_centers = []
+    hist = hist_pdf_orig
+    for i in range(1, hist.GetNbinsX() + 1):  # 1-based bin index
+        center = hist.GetBinCenter(i)
+        bin_centers.append(center)
+    bin_centers = np.array(bin_centers)
+    plt.plot(bin_centers, pdf_cdf, label='PDF CDF')
+    plt.xlabel('mass')
+    plt.ylabel('')
+    plt.legend()
+    plt.grid(True)
+    if save_path != "":
+        plt.savefig(f"{save_path}/GoF_cdfs_SignalFitRange_{cat_name}.pdf")
+    plt.clf()
+    
+    # Draw the normalized pdf histogram:
+    plt.plot(bin_centers, pdf_counts/np.sum(pdf_counts), label='PDF PDF')
+    plt.xlabel('mass')
+    plt.ylabel('')
+    plt.legend()
+    plt.grid(True)
+    if save_path != "":
+        plt.savefig(f"{save_path}/GoF_pdfs_SignalFitRange_{cat_name}.pdf")
+    plt.clf()
+
+
+    # -------------------------------------------
+    # Do KS test
+    # -------------------------------------------
+    return_dict = {}
+    for test_range_name in ["loSB", "hiSB"]:
+        xlow, xhigh = list(x.getRange(test_range_name))
+        data_hist_new = getNewRangeHist(hist_data_orig, f"data_hist_{test_range_name}", xlow, xhigh)
+        pdf_hist_new = getNewRangeHist(hist_pdf_orig, f"pdf_hist_{test_range_name}", xlow, xhigh)
+        data_counts = np.array([data_hist_new.GetBinContent(i) for i in range(1, data_hist_new.GetNbinsX()+1)])
+        pdf_counts = np.array([pdf_hist_new.GetBinContent(i) for i in range(1, pdf_hist_new.GetNbinsX()+1)])
+        # print(f"data_counts: {data_counts}")
+        # print(f"pdf_counts: {pdf_counts}")
+        # nevents=np.sum(data_counts)
+        data_cdf = np.cumsum(data_counts) / np.sum(data_counts)
+        pdf_cdf = np.cumsum(pdf_counts) / np.sum(pdf_counts)
+        ks_statistic = np.max(np.abs(data_cdf - pdf_cdf))
+        print(f"ks_statistic {cat_name} {test_range_name}: {ks_statistic}")
+        nevents = data_hist_new.Integral()
+        
+        return_dict[test_range_name] = {
+            "ks_statistic": ks_statistic,
+            "nevents" : nevents,
+                                       }
+        
+    
+        # Draw the cdf histogram
+        # Extract bin centers (excluding underflow/overflow)
+        bin_centers = []
+        hist = data_hist_new
+        for i in range(1, hist.GetNbinsX() + 1):  # 1-based bin index
+            center = hist.GetBinCenter(i)
+            bin_centers.append(center)
+        bin_centers = np.array(bin_centers)
+        plt.plot(bin_centers, data_cdf, label='Data CDF', linewidth=plot_line_width)
+        plt.plot(bin_centers, pdf_cdf, label='PDF CDF', linewidth=plot_line_width)
+        plt.xlabel('mass')
+        plt.ylabel('')
+        # plt.title('Simple NumPy Plot')
+        plt.legend()
+        plt.grid(True)
+        if save_path != "":
+            plt.savefig(f"{save_path}/GoF_cdfs_{test_range_name}_{cat_name}.pdf")
+        plt.clf()
+
+        # Draw the normalized pdf histogram:
+        plt.plot(bin_centers, data_counts/np.sum(data_counts), label='Data PDF', linewidth=plot_line_width)
+        plt.plot(bin_centers, pdf_counts/np.sum(pdf_counts), label='PDF PDF', linewidth=plot_line_width)
+        plt.xlabel('mass')
+        plt.ylabel('')
+        # plt.title('Simple NumPy Plot')
+        plt.legend()
+        plt.grid(True)
+        if save_path != "":
+            plt.savefig(f"{save_path}/GoF_pdfs_{test_range_name}_{cat_name}.pdf")
+        plt.clf()
+        
+    return return_dict
