2a4d5c39a97baa808741440e0883bf96e79f38cb
ram1123 <ramkrishna.sharma71@gmail.com>
2026-04-08 05:44:53 +0200

    Improvements of naming convention in the ggH stage2 script

--- stage2_sh.sh
+++ stage2_sh.sh
@@ -6,7 +6,7 @@ echo "$(date)"
 # -----------------------------------------------------
 # Configuration
 # -----------------------------------------------------
-label="Run3_nanoAODv12_FilterJetsHorn30GeV_Feb23_tightPassLepVeto_NoJER_AddVars_v2"
+label="Run3_nanoAODv12_FilterJetsHorn25GeV_pySR_Apr03_tightPassLepVeto_NoJER_JetIDFix"
 base_path="/work/projects/hmm/shar1172/hmm_ntuples/copperheadV1clean"
 stage2_load_path="${base_path}/${label}/stage1_output"
 
@@ -15,7 +15,7 @@ category="ggh"
 # model_name="Run3_08March_HPeachFold_train_with_bestHP"
 # model_name="Run3_09March_Check"
 # model_name="Run3_10March_005Trials"
-model_name="Run3_10March_020Trials"
+# model_name="Run3_10March_020Trials"
 # model_name="Run3_10March_020Trials_EBEinTraining" # Run3_10March_020Trials_EBEinTraining
 # model_name="Run3_10March_020Trials_RemovedAddHyperPars"
 # model_name="Run3_10March_100Trials"
@@ -23,6 +23,7 @@ model_name="Run3_10March_020Trials"
 # model_name="Run3_10March_020Trials_oneHotEncoding_Scan"
 # model_name="Run3_09Mar_HPScan_100Trials"
 # model_name="Run3_09Mar_HPScan_500Trials"
+model_name="Run3_07Apr_020Trials_pySR"
 
 step="${1:-}"
 year="${2:-all}"
@@ -34,23 +35,26 @@ if [[ -z "${step}" ]]; then
     exit 1
 fi
 
-model_trainYear="all"
-label_tag="AllYear_17March"
-
-stage2_label="${model_name}_${category}_${label_tag}"
-stage2_save_path="${base_path}/${label}/${stage2_label}/stage2_output"
-
-mva_base_path="${PWD}"
-
 # all_years=(2022preEE 2022postEE 2023 2023BPix 2024 all)
 all_years=(2022preEE 2022postEE 2023 2023BPix 2024)
 # all_years=(all)
 
 if [[ "${year}" == "all" ]]; then
     years=("${all_years[@]}")
+    model_trainYear="allYear"
+    label_tag="AllYear_17March"
 else
     years=("${year}")
+    model_trainYear="${year}"
+    label_tag="${year}_07Apr"
 fi
+
+
+stage2_label="${model_name}_${category}_${label_tag}"
+stage2_save_path="${base_path}/${label}/${stage2_label}/stage2_output"
+
+mva_base_path="${PWD}"
+
 
 # -----------------------------------------------------
 # Helpers
